79c11d8425da1a38ff86726e4b0842c9e8953795
Rishika-Batra <batrarishika06@gmail.com>
2025-05-10 16:51:24 +0530

    Solved RotateIMAGE.java

---
+++ RotateIMAGE.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < row; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
