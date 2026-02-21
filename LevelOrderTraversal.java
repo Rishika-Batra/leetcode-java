8ef1f9ad7c546b758a83dcc60cafdfb11039a4b4
Rishika-Batra <batrarishika06@gmail.com>
2026-02-21 19:05:33 +0530

    Solved Level Order Traversal (BFS)

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class LevelOrderTraversal {
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            result.add(level);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        /*
+                3
+               / \
+              9   20
+                 /  \
+                15   7
+        */
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        List<List<Integer>> result = levelOrder(root);
+
+        System.out.println(result);
+        // Output: [[3], [9, 20], [15, 7]]
+    }
+}
