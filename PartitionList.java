040cf3b5a11e02e71f069230106568d6d5db1747
Rishika-Batra <batrarishika06@gmail.com>
2026-04-30 11:05:33 +0530

    Solved Partition List

---
+++ PartitionList.java
@@ -0,0 +1,62 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class PartitionList {
+
+    public static ListNode partition(ListNode head, int x) {
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+
+        while (head != null) {
+            if (head.val < x) {
+                less.next = head;
+                less = less.next;
+            } else {
+                greater.next = head;
+                greater = greater.next;
+            }
+            head = head.next;
+        }
+
+        // Important: terminate greater list
+        greater.next = null;
+
+        // Connect both lists
+        less.next = greaterDummy.next;
+
+        return lessDummy.next;
+    }
+
+    // Helper to print list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        // 1 -> 4 -> 3 -> 2 -> 5 -> 2
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(2);
+
+        ListNode result = partition(head, 3);
+
+        printList(result); 
+        // Output: 1 -> 2 -> 2 -> 4 -> 3 -> 5 -> null
+    }
+}
